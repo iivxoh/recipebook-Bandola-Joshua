1e744a59723cf1712815aa86ac496bafa0e5d956
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-02-10 00:32:06 +0800

    Added another page for Recipe 2.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import recipes, recipe_one
+from .views import recipes, recipe_one, recipe_two
 
 urlpatterns = [
     path('recipes/list', recipes, name='recipes'),
-    path('recipe/1', recipe_one, name='recipe_one')
+    path('recipe/1', recipe_one, name='recipe_one'),
+    path('recipe/2', recipe_two, name='recipe_two')
 ]
 
 app_name = "ledger"
