fe97981e1907a1a8b28d800e9effc159eba2b5a6
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-03-02 14:06:27 +0800

    Updated the urls for the login view.

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = '/tasks/list'
+LOGOUT_REDIRECT_URL = '/tasks/list'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
