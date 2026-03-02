4c58b5d3a58ea1ebacc7e9101cc34e04a4d5851e
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-03-02 10:46:33 +0800

    Installed the useraccount app. Created the Profile model.

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ledger'
+    'ledger',
+    'useraccounts',
 ]
 
 MIDDLEWARE = [
