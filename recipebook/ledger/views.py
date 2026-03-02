c8001f04ed36d9d024c48ddad60b8eaf3e9f2915
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-03-02 12:06:09 +0800

    Modified the recipe detail template and added a login required.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Recipe
+from django.contrib.auth.decorators import login_required
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
     ctx = {'recipes': recipes}
     return render(request, 'recipes.html', ctx)
 
+@login_required
 def recipe_detail(request, pk):
     recipe = Recipe.objects.get(pk=pk)
     ctx = {
