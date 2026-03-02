a88663c60bc79f35a01b436225f9f2528ccb487d
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-03-02 11:22:33 +0800

    Modified the Recipe model.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from useraccounts.models import Profile
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -12,6 +13,14 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='authors',
+        null=True,
+    )
+    created_on = models.DateTimeField(null=True, auto_now_add=True)
+    updated_on = models.DateTimeField(null=True, auto_now=True)
 
     def __str__(self):
         return self.name
