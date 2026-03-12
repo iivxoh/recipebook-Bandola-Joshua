cd38fef5a4508a530f38f89f7af05f160a96c02a
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-03-12 10:36:06 +0800

    Created a recipe image model.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -39,3 +39,11 @@ class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(
         Recipe, on_delete=models.CASCADE, related_name="ingredients"
     )
+
+
+class RecipeImage(models.Model):
+    recipe_image = models.ImageField(upload_to="images/", null=False)
+    description = models.CharField(max_length=255)
+    imageGroup = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="images"
+    )
