0be58383830c340cb19b4b284974fefdfbcb4575
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-03-12 14:25:33 +0800

    Modified the Recipe admin to contain RecipeImage models.

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = "static/"
 LOGIN_REDIRECT_URL = "/recipes/list"
 LOGOUT_REDIRECT_URL = "/accounts/login"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
